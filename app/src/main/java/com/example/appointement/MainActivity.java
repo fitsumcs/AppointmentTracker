5a4cbde7cc103267bafe789f858952a432c2c329
Fitsum A <fitsum@gmail.com>
2020-10-16 15:47:02 +0300

    converted to a dynamic fragment

--- app/src/main/java/com/example/appointement/MainActivity.java
+++ app/src/main/java/com/example/appointement/MainActivity.java
@@ -37,24 +37,6 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnIt
 
     }
 
-//    @Override
-//    //Returns information passed from addAppointmentactivity
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == 1) {
-//            if(resultCode == RESULT_OK) {
-//                FragmentManager fragmentManager = getSupportFragmentManager();
-//                MainFragment myFragment = (MainFragment) fragmentManager.findFragmentById(R.id.mainFragment);
-//
-//                Appointment myAppointment = new Appointment(
-//                        data.getStringExtra("name"),data.getStringExtra("type"),
-//                        data.getStringExtra("monthOfYear"), data.getIntExtra("dayOfMonth", 0), data.getIntExtra("year", 1111),
-//                        data.getIntExtra("hour", 11),data.getIntExtra("minute", 11),data.getStringExtra("AMorPM"));
-//
-//                myFragment.updateAppointmentListAndDisplay(myAppointment);
-//            }
-//        }
-//    }
 
     @Override
     public void onButtonSelected() {
@@ -88,23 +70,7 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnIt
         ft.commit();
     }
 
-//    @Override
-//    //Returns information passed from addAppointmentactivity
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == 1) {
-//            if(resultCode == RESULT_OK) {
-//
-//                //Creates a new appointment with the information passed
-//                appointmentArrayList.add(new Appointment(
-//                        data.getStringExtra("name"),data.getStringExtra("type"),
-//                        data.getStringExtra("monthOfYear"), data.getIntExtra("dayOfMonth", 0), data.getIntExtra("year", 1111),
-//                        data.getIntExtra("hour", 11),data.getIntExtra("minute", 11),data.getStringExtra("AMorPM")));
-//                //Displays new appointment on in the table
-//                populateTable(appointmentArrayList.size()-1);
-//            }
-//        }
-//    }
+
 
 
 
