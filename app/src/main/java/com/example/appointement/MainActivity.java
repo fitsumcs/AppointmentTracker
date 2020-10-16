b80dc73b70181915cb0f35a6d11f4fd94e6cbedd
Fitsum A <fitsum@gmail.com>
2020-10-16 15:19:14 +0300

    setting the default fragement to be the main

--- app/src/main/java/com/example/appointement/MainActivity.java
+++ app/src/main/java/com/example/appointement/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appointement;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,37 +19,47 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity implements MainFragment.OnItemSelectedListener {
 
 
+    MainFragment myFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fm = getSupportFragmentManager();
+        myFragment = new MainFragment();
+
+        // add
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.myContainer, myFragment);
+        ft.commit();
+
 
     }
 
-    @Override
-    //Returns information passed from addAppointmentactivity
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if(resultCode == RESULT_OK) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                MainFragment myFragment = (MainFragment) fragmentManager.findFragmentById(R.id.mainFragment);
-
-                Appointment myAppointment = new Appointment(
-                        data.getStringExtra("name"),data.getStringExtra("type"),
-                        data.getStringExtra("monthOfYear"), data.getIntExtra("dayOfMonth", 0), data.getIntExtra("year", 1111),
-                        data.getIntExtra("hour", 11),data.getIntExtra("minute", 11),data.getStringExtra("AMorPM"));
-
-                myFragment.updateAppointmentListAndDisplay(myAppointment);
-            }
-        }
-    }
+//    @Override
+//    //Returns information passed from addAppointmentactivity
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 1) {
+//            if(resultCode == RESULT_OK) {
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                MainFragment myFragment = (MainFragment) fragmentManager.findFragmentById(R.id.mainFragment);
+//
+//                Appointment myAppointment = new Appointment(
+//                        data.getStringExtra("name"),data.getStringExtra("type"),
+//                        data.getStringExtra("monthOfYear"), data.getIntExtra("dayOfMonth", 0), data.getIntExtra("year", 1111),
+//                        data.getIntExtra("hour", 11),data.getIntExtra("minute", 11),data.getStringExtra("AMorPM"));
+//
+//                myFragment.updateAppointmentListAndDisplay(myAppointment);
+//            }
+//        }
+//    }
 
     @Override
     public void onButtonSelected() {
 
-            startActivityForResult(new Intent(this,addAppointement.class),1);
+            //startActivityForResult(new Intent(this,addAppointement.class),1);
 
     }
 
